ec5cc952d8516ac85bae776fa5cd396da6844eb3
Ryan Nguyen <ryan.nguyen@students.makeschool.com>
2020-01-28 23:24:33 -0800

    [init]

---
+++ News-API-Take-Home/Controller/HomePageViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomePageViewController.swift
+//  News-API-Take-Home
+//
+//  Created by Ryan Nguyen on 1/28/20.
+//  Copyright © 2020 Danh Phu Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class HomePageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+
+        // Do any additional setup after loading the view.
+    }
+
+}
